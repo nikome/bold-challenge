361fddfcf0373b18b6ef3f7732ed6a051affc08e
nikome <nicolasmunoz.ocm@gmail.com>
2021-07-17 11:24:01 -0500

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
 import React from 'react';
 import styled from 'styled-components';
-import {ReactComponent as Example} from './logoBold.svg';
+import Logo from './logo.png';
 
+const BoldLogo = styled.div`
+  width: 150px;
+  height: 60px;
+  background-image: url(${Logo});
+`;
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <div>
+          <BoldLogo />
           <a>1</a>
           <a>2</a>
         </div>
