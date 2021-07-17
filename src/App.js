0bf5e0d3e9ab205718c45d414ff0eb8208af89d2
nikome <nicolasmunoz.ocm@gmail.com>
2021-07-16 23:10:36 -0500

    First commit, new app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <a>1</a>
+          <a>2</a>
+        </div>
+      </header>
+      <div>
+        <div>-----Total ventas-----</div>
+        <div>+++++Filtros+++++</div>
+      </div>
+      <div>
+        Tabla de ventas
+      </div>
+    </div>
+  );
+}
+
+export default App;
